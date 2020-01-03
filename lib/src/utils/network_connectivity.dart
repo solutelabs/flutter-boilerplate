9cd43cea0f6c1d777e66cf0ca578344f68d85b8e
Milind Mevada <milind.mevada@solutelabs.com>
2020-01-03 18:25:47 +0530

    Added Network Connectivity checker

---
+++ lib/src/utils/network_connectivity.dart
@@ -0,0 +1,39 @@
+import 'package:connectivity/connectivity.dart';
+import 'package:flutter_base_project/src/utils/custom_exceptions.dart';
+
+abstract class NetworkConnectivityChecker {
+  Future<bool> isNetworkAvailable();
+
+  Future<void> validateNetworkConnectivity();
+}
+
+NetworkConnectivityChecker providerNetworkConnectivityChecker() {
+  return NetworkConnectivity(Connectivity());
+}
+
+class NetworkConnectivity implements NetworkConnectivityChecker {
+  Connectivity connectivity;
+
+  NetworkConnectivity(this.connectivity);
+
+  @override
+  Future<bool> isNetworkAvailable() async {
+    final result = await connectivity.checkConnectivity();
+    return _isNetworkAvailable(result);
+  }
+
+  @override
+  Future<void> validateNetworkConnectivity() async {
+    final result = await connectivity.checkConnectivity();
+    if (!_isNetworkAvailable(result)) {
+      throw NoNetworkException();
+    }
+  }
+
+  bool _isNetworkAvailable(ConnectivityResult result) {
+    return [
+      ConnectivityResult.mobile,
+      ConnectivityResult.wifi,
+    ].contains(result);
+  }
+}
